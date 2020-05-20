5db530ec651ad8ad48af0673b04688cf20e41dfe
zakio <yu.m1wd11@@gmail.com>
2020-05-20 12:35:35 +0900

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,12 @@ class Post < ApplicationRecord
 
   validates :title, :content, presence: true, unless: :image?
 
+  mount_uploader :image, ImageUploader
+
 
   def self.search(search)
     return Post.all unless search
     Post.where('title LIKE(?)', "%#{search}%")
   end
+
 end
